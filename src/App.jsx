28fd1c4e43d94a74fcdecd53459762f378c121ad
Smah Riki <86798574+sr1k1@users.noreply.github.com>
2026-03-04 14:33:39 -0800

    imported pokemon sets data and created state for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import { useState } from "react";
+import { useState, useReducer, useEffect } from "react";
 import "./App.css";
 
+// Import objects related to state management
+import {
+  reducer as calcReducer,
+  actions as calcActions,
+  initialState as calcInitialState,
+} from "./reducers/pkmnCalc.reducer.js";
+
+// Delete below import when ready to fetch from server
+import { pokemon } from "./temp_data/gen9ou.json";
+
 function App() {
-  const [count, setCount] = useState(0);
+  // Data fetch from showdown API
+  const [calcState, dispatch] = useReducer(calcReducer, calcInitialState);
+
+  // Uncomment below when we are ready to fetch data straight from server
+  // useEffect(() => {
+  //   async function fetchPokemonSetData() {
+  //     const response = await fetch("https://data.pkmn.cc/stats/gen9ou.json");
+  //     const { pokemon } = await response.json();
+  //     dispatch({ type: calcActions.setPkmnPool, pokemon });
+
+  //     return;
+  //   }
+  //   fetchPokemonSetData();
+  // }, []);
+
+  // Delete lines below when we are ready to fetch from server
+  useEffect(() => {
+    dispatch({ type: calcActions.setPkmnPool, pokemon });
+  }, []);
+
+  // --------------------------------------------------------- //
 
   return <></>;
 }
